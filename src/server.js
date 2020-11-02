36d5943471ef8d7a9de3f10bcffed97f2e67eb36
MarianaIngridC <maring.calle@gmail.com>
2020-11-02 20:59:37 -0300

    version1

--- src/server.js
+++ src/server.js
@@ -36,7 +36,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
 }
 
-
 app.listen(process.env.PORT, () => {
     console.log(`El servidor esta corriendo en el puerto ${process.env.PORT}`)
 })
